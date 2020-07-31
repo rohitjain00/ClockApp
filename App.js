8c75771bf4ed9d4aac37b2ddb87e348deec4db86
Rohit Jain <rohitjain18005@gmail.com>
2020-07-31 21:36:54 +0530

    Removed ClockComponent.js due to extra component

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {ClockComponent} from './components/ClockComponent';
+import {ClockController} from "./components/ClockController";
 
 export default function App() {
   return (
-    <ClockComponent></ClockComponent>
+    <ClockController></ClockController>
   );
 }
--- components/ClockComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {ClockController} from './ClockController';
-
-export function ClockComponent() {
-  return (
-    <ClockController></ClockController>
-  );
-}
-
