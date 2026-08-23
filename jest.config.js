27fdb1662ba2d7df7630a54dd38fbba09a761b0b
Rohit Jain <rohitjain00@users.noreply.github.com>
2026-08-24 01:30:06 +0530

    feat: upgrade to Expo SDK 57 (consolidated dep alignment)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "jest-expo",
-  setupFilesAfterEnv: ["@testing-library/react-native/extend-expect"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ],
